32638e8e4bc3481ed0cdd05ba2d6034bf689b66e
Farahnush <nfarahnush@gmail.com>
2022-01-23 23:59:30 +0500

    correction

--- index.php
+++ index.php
@@ -11,30 +11,30 @@
 4 shrimps
 */
 
-function cookAQurutob
-(float $fatir,
- float $chaka,
- float $salt,
- float $tomatoes,
- float $onions, 
- float $springOnions,
- float $cilantro,
- float $VegetableOil,
- float $shrimps,
- string $horoshieSlova)
+function cookQurutob
+(int $fatir,
+ int $chaka,
+ int $salt,
+ int $tomatoes,
+ int $onions,
+ int $spring_onions,
+ int $cilantro,
+ int $vegetable_oil,
+ int $shrimps,
+ string $horoshie_slova)
 {
-    echo "1. Saute $onions onions with $shrimps shrimps in a skillet in hot $VegetableOiloil oil until translucent, about 8 minutes.<br> 
+    echo "1. Saute $onions onions with $shrimps shrimps in a skillet in hot $vegetable_oil oil until translucent, about 8 minutes.<br> 
 2. Meanwhile, place small pieces of $fatir bread in traditionally a large wooden dish.<br> 
 3. Top with $chaka chaka. <br>
 4. Pour the onion with the hot oil. <br>
-5. Top with $springOnions spring onions, $tomatoes tomatoes and $cilantro cilantro. Serve immediately. <br>
-$horoshieSlova";
+5. Top with $spring_onions spring onions, $tomatoes tomatoes and $cilantro cilantro. Serve immediately. <br>
+$horoshie_slova";
     /*Saute onion in a skillet in hot oil until translucent, about 8 minutes.
 Meanwhile, place small pieces of fatir bread in traditionally a large wooden dish.
 Top with chaka. Pour the onion with the hot oil. Top with spring onions, tomatoes and cilantro. Serve immediately.*/
 }
 
-cookAQurutob(1,300,2,3,2,2,1,100,4,'Bon appetit');
+cookQurutob(1, 300, 2, 3, 2, 2, 1, 100, 4, 'Bon appetit');
 
 
 
